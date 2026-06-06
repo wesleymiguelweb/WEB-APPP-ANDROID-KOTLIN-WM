019a20450955ec07fe980360397298087bbc76d8
Wesley Miguel <wesley.miguel7@outlook.com>
2026-06-06 15:01:18 -0300

    Atualização

--- app/src/main/java/com/example/filmesplay/RetrofitClient.kt
+++ app/src/main/java/com/example/filmesplay/RetrofitClient.kt
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitClient {
     // Para o emulador padrão do Android Studio use: http://10.0.2.2/api/
     // Para o Genymotion use: http://10.0.3.2/api/ ou o IP da sua máquina
-    private const val BASE_URL = "http://192.168.3.54/api/"
+    private const val BASE_URL = "http://10.0.0.104/api/"
 
     val api: ApiService by lazy {
         Retrofit.Builder()
